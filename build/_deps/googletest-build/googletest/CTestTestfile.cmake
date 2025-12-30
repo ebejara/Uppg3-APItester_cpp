4b14d664042251ce142071970673913e01fabc75
ebejara <enrique@bejaranomail.com>
2025-12-31 00:19:27 +0100

    Remove build folder from repo (added by mistake)

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Enrique/Documents/VScode/CI/Uppg3-APItester_cpp/build/_deps/googletest-src/googletest
-# Build directory: C:/Users/Enrique/Documents/VScode/CI/Uppg3-APItester_cpp/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
